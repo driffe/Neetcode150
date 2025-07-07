8ae2c4d275a4023a4299460be092e77e69e408ce
SeyoungOh <33598679+driffe@users.noreply.github.com>
2025-07-06 22:00:55 -0700

    stduy

--- src/TwoPointer.java
+++ src/TwoPointer.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 public class TwoPointer {
     public boolean isPalindrome(String s) {
@@ -131,6 +132,63 @@ public class TwoPointer {
         }
 
         return result;
+    }
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        char[] parentheses = s.toCharArray();
+
+        for(char c : parentheses) {
+            if(c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')') {
+                if(stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            } else if (c == '}') {
+                if(stack.isEmpty() || stack.pop() != '{') {
+                    return false;
+                }
+            } else if (c == ']') {
+                if(stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+    private int min;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+        min = 0;
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if(minStack.isEmpty() || val < minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        if(!stack.isEmpty()) {
+            int temp = stack.pop();
+            if (temp == minStack.peek()) {
+                minStack.pop();
+            }
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
     }
 
 }
